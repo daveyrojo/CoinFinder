6e99daecac390020964bb30cff4be7d7834b72d5
Michael DiGiacomo <mdigiacomo05@me.com>
2021-02-17 13:44:49 -0500

    minor changes regarding the map

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// function initMap() {
+//     var newJersey = { lat: 40.0583, lng: -74.4057 };
+//     var map = new google.maps.Map(document.getElementById("map"), {
+//       zoom: 6,
+//       center: newJersey,
+//     });
+//     var marker = new google.maps.Marker({
+//       position: newJersey,
+//       map: map,
+//     });
+//   }
