9ff1b0be4af0997b8da10a7d0999b676e6f6cde0
David Eldridge <d.vincenteldridge@gmail.com>
2021-02-16 21:28:00 -0500

    added global variables for search, and userInput search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//get search query on button click
+//get search query on button click **done
 //get google maps sub function in main function
 //produce on page
 //make dynamic variables//have search query work with variables
@@ -9,12 +9,33 @@
     
 // })
 
-//var userLat = <ui>
-//var maxLat = userLat + 1
+//latitude = 69 miles
+//longitude = 54.6 miles
 var searchButton = document.querySelector('#searchButton');
 
+var minLat = 40;
+var maxLat = 42;
+var minLon = -75;
+var maxLon = -74;
+// var searchRadius = 1;
+
+var userInput = function(userLat, userLon, searchRadius) {
+    if (searchRadius <= 0) {
+        console.log('Radius is not positive');
+        return;
+    }
+    minLat = userLat - searchRadius/69.0;
+    maxLat = userLat + searchRadius/69.0;
+    minLon = userLon - searchRadius/54.6;
+    maxLon = userLon + searchRadius/54.6;
+        
+}
+
 searchButton.addEventListener('click', function() {
-    fetch('https://cors-anywhere.herokuapp.com/https://coinmap.org/api/v1/venues/?lat1=40&lon1=-75&lat2=42&lon2=-74',)
+    var searchQuery = 'https://cors-anywhere.herokuapp.com/https://coinmap.org/api/v1/venues/?lat1=' + minLat.toFixed(4) + '&lon1=' + minLon.toFixed(4) + '&lat2=' + maxLat.toFixed(4) + '&lon2=' + maxLon.toFixed(4);
+    console.log(minLat, maxLat, minLon, maxLon);
+    console.log(searchQuery);
+    fetch(searchQuery)
         .then(function (response) {
             console.log(response);
             return response.json();
@@ -32,8 +53,9 @@ searchButton.addEventListener('click', function() {
                 console.log(venueObj[i].lon);
             }
         })
-
 })
+
+userInput(40, -72, 50);
 
 
 
