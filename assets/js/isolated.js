92291c451bb3248dd74df875429f10c96c237760
David Eldridge <d.vincenteldridge@gmail.com>
2021-02-18 09:59:25 -0500

    added a note

--- assets/js/isolated.js
+++ assets/js/isolated.js
@@ -20,8 +20,8 @@ var userInput = function (userLat, userLon, searchRadius) {
 }
 
 searchAddress.addEventListener('click', function getLatLon(userAddress) {
-
-    var location= '210 Cambridge Ave Jersey City, NJ';
+        //need to change to a variable
+    var location = '210 Cambridge Ave Jersey City, NJ';
     axios.get('https://maps.googleapis.com/maps/api/geocode/json?', {params:{
         address: location,
         key: 'AIzaSyDwit_ck5hR1Olc9m0xDLOuZmRaJTZN8F4'
