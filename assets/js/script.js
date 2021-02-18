0a83dc59f82fec8f9a8f0db89c9e3014786f62af
Michael DiGiacomo <mdigiacomo05@me.com>
2021-02-18 14:55:08 -0500

    n/a

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-// function initMap() {
-//     var newJersey = { lat: 40.0583, lng: -74.4057 };
-//     var map = new google.maps.Map(document.getElementById("map"), {
-//       zoom: 6,
-//       center: newJersey,
-//     });
-//     var marker = new google.maps.Marker({
-//       position: newJersey,
-//       map: map,
-//     });
-//   }
+ScrollSpeed = 100
+ScrollChars = 1
+function ScrollMarquee() {
+window.setTimeout('ScrollMarquee()',ScrollSpeed);
+
+var msg = document.marquee1.text.value;
+document.marquee1.text.value =
+msg.substring(ScrollChars) +
+msg.substring(0,ScrollChars);
+}
+ScrollMarquee()
