092e93203c65b543f3768e00883f282e37cf5b5e
James VanderKam <jamesvanderkam@verizon.net>
2021-02-18 12:38:26 -0500

    locally stores search radius

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,30 @@ var minLon = -75;
 var maxLon = -74;
 var lat = 0;
 var lon =0;
-var searchRadius = 1;
+if(!localStorage.getItem("coinRadius")){
+  localStorage.setItem("coinRadius", 1);
+}
 
-var userInput = function(userLat, userLon, radius) {
-    if (searchRadius <= 0) {
+
+/* var init = function(){
+  //fetch from url here
+  console.log("init started")
+  var apiKey = config.API_KEY;
+  //fetch("https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/js?key="+apiKey+"&callback=initMap&libraries=places&v=weekly")
+  axios.get("https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/js?key="+apiKey+"&callback=initMap&libraries=places&v=weekly")
+  .then(function(gmaps){
+    console.log("gmaps is",gmaps)
+  })
+}
+init(); */
+
+//given a latitude and longitude, and the current radius, generate 4 corners
+var userInput = function(userLat, userLon) {
+    if (localStorage.getItem("coinRadius") <= 0) {
         console.log('Radius is not positive');
         return;
     }
+    searchRadius = localStorage.getItem("coinRadius")
     console.log(userLat+" "+userLon+" "+searchRadius);
     minLat = userLat - searchRadius/69.0;
     maxLat = userLat + searchRadius/69.0;
@@ -47,11 +64,11 @@ searchButton.addEventListener('click', function() {
             venueObj = data.venues;
             for (let i = 0; i < venueObj.length; i++) {
                var venueName = venueObj[i].name;
-               console.log(venueName);
-               console.log('-----------------');
-               console.log(venueObj[i].lat);
-               console.log('-----------------');
-               console.log(venueObj[i].lon);
+              //  console.log(venueName);
+              //  console.log('-----------------');
+              //  console.log(venueObj[i].lat);
+              //  console.log('-----------------');
+              //  console.log(venueObj[i].lon);
                addMarker(venueObj[i].lat, venueObj[i].lon,i+" ",venueName);
             }
 
@@ -59,11 +76,44 @@ searchButton.addEventListener('click', function() {
 })
 
 
+var searchAddress = document.querySelector('#searchAddress');
+//needs to be made for input field
+var userAddress = '210 Cambridge Ave Jersey City, NJ';
+
+searchAddress.addEventListener('click', function getLatLon(userAddress) {
+    //need to change to a variable
+    var location = '210 Cambridge Ave Jersey City, NJ';
+    axios.get('https://maps.googleapis.com/maps/api/geocode/json?', {
+        params: {
+            address: location,
+            key: process.env.APIKEY
+        }
+    })
+        .then(function (response) {
+            // console.log(response);
+            let googleData = response.data;
+            // console.log(googleData);
+            let locationData = googleData.results;
+            // console.log(locationData);
+            let addressCode = locationData[0];
+            // console.log(addressCode);
+            let geoCode = addressCode.geometry;
+            // console.log(geoCode);
+            let coords = geoCode.location;
+            // console.log(coords);
+            let userLat = coords.lat;
+            let userLon = coords.lng;
+            console.log(userLat, userLon);
+
+        })
+});
 
 var map;
 var autocomplete;
  //initializes the map
  function initMap() {
+  //fetch from url here
+
   var eastCoast = { lat: 40.0, lng: -72.0 };
   console.log("initializing map");
   map = new google.maps.Map(document.getElementById("map"), {
@@ -83,7 +133,7 @@ autocomplete.addListener("place_changed", addCustomerLocation);
 }
 
   var addMarker = function(latIn, lngIn, labIn, name){ //Adds a new location with a given name, label, and location data
-    console.log("Adding marker at "+latIn+" "+lngIn+" "+labIn+" "+name);
+    //console.log("Adding marker at "+latIn+" "+lngIn+" "+labIn+" "+name);
     var marker = new google.maps.Marker({
         position: { lat: latIn, lng: lngIn},
         label:labIn,
@@ -100,7 +150,7 @@ autocomplete.addListener("place_changed", addCustomerLocation);
 //gets an inputed address
 function addCustomerLocation() { //Gets the location from the user's saved file
   // Get the place details from the autocomplete object.
-  console.log("customer location added?");
+  //console.log("customer location added?");
     const userPlace = autocomplete.getPlace();
 
 
@@ -119,7 +169,7 @@ function addCustomerLocation() { //Gets the location from the user's saved file
     lat = userPlace.geometry.location[0];
     lon = userPlace.geometry.location[1];
     console.log(lat+" "+lon+ " "+ userPlace.geometry.location+" "+typeof(userPlace.geometry.location) +" "+userPlace.geometry.location.value);
-    userInput(lat, lon, searchRadius);
+    userInput(lat, lon);
     return [userPlace.geometry.location.latitude,userPlace.geometry.location.longitude ];
 }
 
@@ -129,12 +179,12 @@ locButton.addEventListener("click", getLocation);
 //gets html location
 function getLocation() {
 
-    console.log("getting location");
-    if(radInput.value){
-        
-        searchRadius = radInput.value;
-        console.log(searchRadius);
-    }
+  console.log("getting location");
+  if(radInput.value){
+      
+      localStorage.setItem("coinRadius",radInput.value);
+      console.log(localStorage.getItem("coinRadius"));
+  }
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(returnPosition);
     
@@ -151,7 +201,7 @@ function returnPosition(position){
     //console.log("in return "+position.coords.latitude+" "+position.coords.longitude);
     lat = position.coords.latitude;
     lon = position.coords.longitude;
-    userInput(position.coords.latitude, position.coords.longitude, searchRadius);
+    userInput(position.coords.latitude, position.coords.longitude);
     return [position.coords.latitude, position.coords.longitude];//seems not to return?
 }
 
